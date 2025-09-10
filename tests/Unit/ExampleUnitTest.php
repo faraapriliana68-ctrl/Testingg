a64a99970a93c6d932e519302ed6d3c55d6c071e
Nama Kamu <email@example.com>
2025-09-10 21:37:44 +0800

    initial commit - Laravel project + test

---
+++ tests/Unit/ExampleUnitTest.php
@@ -0,0 +1,15 @@
+<?php 
+    namespace Test\Unit;
+    use PHPUnit\Framework\TestCase;
+    class ExampleUnitTest extends TestCase
+{
+     /** @test */
+public function it_can_add_two_numbers()
+{
+$a = 5;
+$b = 10;
+$result = $a + $b ;
+
+$this->assertEquals(15,$result);
+}
+}
